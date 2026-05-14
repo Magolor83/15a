5316f5007cc0dc2e0c089d6c6bc976ce62c03c7a
Thomas Israel <tisrael@Mac.fritz.box>
2026-05-14 21:53:06 +0200

    Add daily caffeine calendar sync

---
+++ pregnancy-firebase-config.js
@@ -0,0 +1,11 @@
+window.PREGNANCY_DASHBOARD_ID = "pregnancy-caffeine";
+window.PREGNANCY_SHARED_EMAIL = "reiseplan@reiseplan-559d3.web.app";
+
+window.PREGNANCY_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAeYfBOwBKc8ZYafoHeoMphl00F8iFX2LM",
+  authDomain: "reiseplan-559d3.firebaseapp.com",
+  projectId: "reiseplan-559d3",
+  storageBucket: "reiseplan-559d3.firebasestorage.app",
+  messagingSenderId: "1004060232207",
+  appId: "1:1004060232207:web:b3f78b978db4182b7bf17a"
+};
